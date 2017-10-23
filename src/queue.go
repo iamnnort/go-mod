35e4770188ae58eb152988d8de2813764a8a7d8a
nikitapavets <nikitapavets@gmail.com>
2017-10-23 16:34:32 +0300

    add queue

---
+++ src/queue.go
@@ -0,0 +1,31 @@
+package main
+
+// Modeling system queue
+type Queue struct {
+	maxItems int
+	items    int
+}
+
+func (queue *Queue) addItem() {
+	if queue.items < queue.maxItems {
+		queue.items++
+	}
+}
+
+func (queue *Queue) removeItem() {
+	if queue.items > 0 {
+		queue.items--
+	}
+}
+
+func (queue *Queue) isFull() bool {
+	return queue.items == queue.maxItems
+}
+
+func (queue *Queue) isEmpty() bool {
+	return queue.items == 0
+}
+
+func (queue *Queue) getValue() int {
+	return queue.items
+}
